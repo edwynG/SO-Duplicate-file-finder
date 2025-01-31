8e487dc323ccc353c960fa98d5c6b030b3c07b1a
edwynG <edwynjesus30@gmail.com>
2025-01-31 16:45:28 -0400

    comentarios

--- include/utils.h
+++ include/utils.h
@@ -16,12 +16,12 @@ char getType(unsigned int mode);
 // Agrega a una lista todas los archivos y carpetas de un directorio
 // @param DirectoryName ruta del directorio
 // @param toVisit lista a usar
+// @param Modo de calculo de hash
 void directoryTour(char *DirectoryName, struct List *toVisit, char funcMode);
 
 // Verifica si un archivo pertence a una categoría o partición. Si pertenece, indica la categoria mediante un parametro por referencia
 // @param list Un puntero a la estructura List
 // @param value Una cadena que representa el valor a verificar
-// @param type Un caracter que representa el modo de hash a usar
 // @param parentNode Un apuntador a un puntero de una estructura FilesDuplicates que representará la categoria a la que pertenezca el archivo
 // @return (int) que indica si la categoría está incluida (1) o no (0)
 int isIncludedCategory(struct List *listCategory, char *hashCompare, struct FilesDuplicates **parentNode);
